a2d94be5274ede2452e9bd496084067399c960cd
Luis Andrés Rodríguez Santoyo <andres.rodriguez_94@outlook.com>
2017-07-11 12:21:45 -0500

    Initial Commit

---
+++ U-Broker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  U-Broker
+//
+//  Created by Luis Andrés Rodríguez Santoyo on 11/07/17.
+//  Copyright © 2017 Grupo Arquimo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
